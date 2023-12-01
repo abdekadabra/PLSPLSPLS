41a52d7fe0e4ead976a288f26250a15497b229c2
abdekadabra <146800513+abdekadabra@users.noreply.github.com>
2023-12-01 11:15:09 +0300

    Add files via upload

---
+++ HTMLGenerator/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.editImageButton = new System.Windows.Forms.Button();
+            this.addImageButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.bodyTextBox = new System.Windows.Forms.TextBox();
+            this.Generate = new System.Windows.Forms.Button();
+            this.headTextBox = new System.Windows.Forms.TextBox();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.questionButton = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.questionButton);
+            this.panel1.Controls.Add(this.editImageButton);
+            this.panel1.Controls.Add(this.addImageButton);
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Controls.Add(this.bodyTextBox);
+            this.panel1.Controls.Add(this.Generate);
+            this.panel1.Controls.Add(this.headTextBox);
+            this.panel1.Controls.Add(this.titleTextBox);
+            this.panel1.Controls.Add(this.webBrowser);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1475, 629);
+            this.panel1.TabIndex = 0;
+            // 
+            // editImageButton
+            // 
+            this.editImageButton.Location = new System.Drawing.Point(772, 0);
+            this.editImageButton.Name = "editImageButton";
+            this.editImageButton.Size = new System.Drawing.Size(84, 51);
+            this.editImageButton.TabIndex = 9;
+            this.editImageButton.Text = "Изменить изображение";
+            this.editImageButton.UseVisualStyleBackColor = true;
+            this.editImageButton.Click += new System.EventHandler(this.editImageButton_Click);
+            // 
+            // addImageButton
+            // 
+            this.addImageButton.Location = new System.Drawing.Point(682, 0);
+            this.addImageButton.Name = "addImageButton";
+            this.addImageButton.Size = new System.Drawing.Size(84, 51);
+            this.addImageButton.TabIndex = 8;
+            this.addImageButton.Text = "Добавить изображение";
+            this.addImageButton.UseVisualStyleBackColor = true;
+            this.addImageButton.Click += new System.EventHandler(this.addImageButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(3, 300);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(675, 266);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // bodyTextBox
+            // 
+            this.bodyTextBox.Location = new System.Drawing.Point(0, 53);
+            this.bodyTextBox.Multiline = true;
+            this.bodyTextBox.Name = "bodyTextBox";
+            this.bodyTextBox.Size = new System.Drawing.Size(678, 241);
+            this.bodyTextBox.TabIndex = 6;
+            this.bodyTextBox.TextChanged += new System.EventHandler(this.bodyTextBox_TextChanged);
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(1388, 29);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(75, 23);
+            this.Generate.TabIndex = 4;
+            this.Generate.Text = "Сохранить";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // headTextBox
+            // 
+            this.headTextBox.Location = new System.Drawing.Point(3, 29);
+            this.headTextBox.Name = "headTextBox";
+            this.headTextBox.Size = new System.Drawing.Size(368, 20);
+            this.headTextBox.TabIndex = 3;
+            this.headTextBox.TextChanged += new System.EventHandler(this.headTextBox_TextChanged);
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(3, 3);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(368, 20);
+            this.titleTextBox.TabIndex = 2;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(682, 53);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(782, 513);
+            this.webBrowser.TabIndex = 1;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // questionButton
+            // 
+            this.questionButton.Location = new System.Drawing.Point(862, 24);
+            this.questionButton.Name = "questionButton";
+            this.questionButton.Size = new System.Drawing.Size(121, 23);
+            this.questionButton.TabIndex = 10;
+            this.questionButton.Text = "Почему ошибка?";
+            this.questionButton.UseVisualStyleBackColor = true;
+            this.questionButton.Click += new System.EventHandler(this.questionButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(377, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "←Название документа(Title)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(378, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "←Заголовок (Head)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1475, 625);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox headTextBox;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.TextBox bodyTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button addImageButton;
+        private System.Windows.Forms.Button editImageButton;
+        private System.Windows.Forms.Button questionButton;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HTMLGenerator/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string currentImagePath;
+
+        private ToolTip toolTip;
+        private Timer tooltipTimer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Инициализируем HTML-документ при запуске формы
+            UpdateHtmlDocument();
+
+            // Подключаем событие KeyDown для bodyTextBox
+            bodyTextBox.KeyDown += bodyTextBox_KeyDown;
+
+            // Очистим PictureBox
+            pictureBox.Image = null;
+
+            // Инициализация ToolTip
+            toolTip = new ToolTip();
+            toolTip.AutoPopDelay = 1000; // Время отображения подсказки
+            toolTip.InitialDelay = 5000; // Начальная задержка перед отображением подсказки
+            toolTip.ReshowDelay = 100; // Задержка перед повторным отображением подсказки
+            toolTip.ShowAlways = false; // Устанавливаем false, чтобы ToolTip не отображался всегда
+
+            // Инициализация Timer
+            tooltipTimer = new Timer();
+            tooltipTimer.Interval = 5000; // Интервал в миллисекундах (в данном случае, 5 секунд)
+            tooltipTimer.Tick += TooltipTimer_Tick; // Обработчик события срабатывания таймера
+
+            // Добавляем подсказку к кнопке
+            toolTip.SetToolTip(questionButton, "Путь к изображение и само изображение не должны содержать пробелов");
+
+            questionButton.Click += questionButton_Click;
+
+        }
+        private void questionButton_Click(object sender, EventArgs e)
+        {
+            // Показываем подсказку при клике на кнопку
+            toolTip.Show("Используйте изображение без пробелов в названии", questionButton, 0, questionButton.Height, 5000);
+            tooltipTimer.Start(); // Запускаем таймер
+        }
+        private void TooltipTimer_Tick(object sender, EventArgs e)
+        {
+            // Скрываем подсказку и останавливаем таймер
+            toolTip.Hide(questionButton);
+            tooltipTimer.Stop();
+        }
+        private void UpdateHtmlDocument()
+        {
+            // Получаем данные из TextBox'ов и ComboBox
+            string title = titleTextBox.Text;
+            string headContent = headTextBox.Text;
+            string bodyContent = bodyTextBox.Text; // Добавлено получение содержимого bodyTextBox
+
+            // Формируем HTML - код
+            string htmlCode = GenerateHtmlDocument(title, headContent, bodyContent);
+
+
+            // Отображаем HTML-код в WebBrowser
+            webBrowser.DocumentText = htmlCode;
+        }
+
+        private string GenerateHtmlDocument(string title, string headContent, string bodyContent)
+        {
+            // Формируем HTML-код
+            StringBuilder htmlBuilder = new StringBuilder();
+
+            htmlBuilder.AppendLine("<!DOCTYPE html>");
+            htmlBuilder.AppendLine("<html>");
+
+            // Начинаем <head>
+            htmlBuilder.AppendLine("<head>");
+            htmlBuilder.AppendLine("<meta charset=\"utf-8\">");
+
+            // Добавляем иконку сайта
+            htmlBuilder.AppendLine("<link rel=\"icon\" href=\"favicon.ico\" type=\"image/x-icon\">");
+
+            // Здесь добавьте стили, если необходимо
+
+            // Вставляем заголовок из TextBox'а внутри <head>
+            htmlBuilder.AppendLine($"<title>{title}</title>");
+
+            // Закрываем <head>
+            htmlBuilder.AppendLine("</head>");
+
+            // Начинаем <body>
+            htmlBuilder.AppendLine("<body>");
+
+            // Вставляем заголовок внутри <body>
+            htmlBuilder.AppendLine($"<h1>{headContent}</h1>");
+
+            // Вставляем содержимое bodyTextBox внутри <body>
+            htmlBuilder.AppendLine($"<p>{bodyContent}</p>");
+
+            // Здесь вы можете добавить другие элементы внутрь <body>
+
+            // Закрываем <body>
+            htmlBuilder.AppendLine("</body>");
+
+            // Закрываем <html>
+            htmlBuilder.AppendLine("</html>");
+
+            return htmlBuilder.ToString();
+        }
+
+
+
+        private void SaveHtmlToFile(string htmlCode, string fileName)
+        {
+            try
+            {
+                // Получаем путь к папке, в которой находится HTML-файл
+                string folderPath = Path.GetDirectoryName(fileName);
+
+                // Копируем изображение в ту же папку
+                string newImagePath = Path.Combine(folderPath, Path.GetFileName(currentImagePath));
+                File.Copy(currentImagePath, newImagePath, true);
+
+                // Заменяем путь к изображению в HTML-коде на абсолютный
+                htmlCode = htmlCode.Replace(currentImagePath, newImagePath);
+
+                // Сохраняем HTML-документ в файл
+                using (StreamWriter sw = new StreamWriter(fileName))
+                {
+                    sw.Write(htmlCode);
+                }
+
+                MessageBox.Show($"HTML-документ сохранен в файл: {fileName}", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при сохранении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Получаем данные из TextBox'ов
+            string title = titleTextBox.Text;
+            string headContent = headTextBox.Text;
+            string bodyContent = bodyTextBox.Text; // Добавлено получение содержимого bodyTextBox
+
+
+            // Формируем HTML-код
+            string htmlCode = GenerateHtmlDocument(title, headContent, bodyContent);
+
+            // Сохраняем HTML-код в файл
+            SaveHtmlToFile(htmlCode, "output.html");
+        }
+
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void bodyTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // При изменении текста в bodyTextBox обновляем HTML-документ
+            UpdateHtmlDocument();
+        }
+
+        private void headTextBox_TextChanged(object sender, EventArgs e)
+        {
+            UpdateHtmlDocument();
+        }
+
+        private void bodyTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Проверяем, были ли нажаты клавиши Shift и Enter
+            if (e.Shift && e.KeyCode == Keys.Enter)
+            {
+                // Добавляем тег переноса строки в текстовое поле
+                bodyTextBox.SelectedText = "<br>";
+
+                // Предотвращаем дальнейшую обработку события KeyDown
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            // Открывайте изображение во внешнем редакторе, если оно выбрано
+            if (!string.IsNullOrEmpty(currentImagePath) && File.Exists(currentImagePath))
+            {
+                System.Diagnostics.Process.Start("mspaint.exe", currentImagePath);
+            }
+        }
+
+        private void addImageButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Изображения (*.jpg; *.jpeg; *.png; *.gif)|*.jpg; *.jpeg; *.png; *.gif|Все файлы (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Получаем выбранный путь к файлу изображения
+                    currentImagePath = openFileDialog.FileName;
+
+                    // Отображаем изображение в PictureBox
+                    pictureBox.Image = Image.FromFile(currentImagePath);
+
+                    // Добавляем тег <img> с выбранным изображением в bodyTextBox
+                    bodyTextBox.SelectedText = $"<img src=\"{currentImagePath}\"";
+                }
+            }
+        }
+
+        private void editImageButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentImagePath))
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Изображения (*.jpg; *.jpeg; *.png; *.gif)|*.jpg; *.jpeg; *.png; *.gif|Все файлы (*.*)|*.*";
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Получаем новый путь к файлу изображения
+                        string newImagePath = openFileDialog.FileName;
+
+                        // Заменяем тег <img> в bodyTextBox на новый путь к изображению и обновленные размеры
+                        bodyTextBox.Text = Regex.Replace(
+                            bodyTextBox.Text,
+                            $"<img src=\"{Regex.Escape(currentImagePath)}\" alt=\"Описание изображения\"(.*?)>",
+                            $"<img src=\"{newImagePath}\" alt=\"Описание изображения\" style=\"width:{pictureBox.Image?.Width}px; height:{pictureBox.Image?.Height}px;\">"
+                        );
+
+                        // Обновляем текущий путь к изображению
+                        currentImagePath = newImagePath;
+                    }
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Выберите изображение для редактирования.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
